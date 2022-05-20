8c21be00fc8579d843f9f58ee835b437f25f2810
anniasebold <anniasebold3.0@gmail.com>
2022-05-20 00:53:27 -0300

    Initial commit

---
+++ optmize.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const AWS = require("aws-sdk");
+const sharp = require("sharp");
+const { basename, extname } = require("path");
+
+const S3 = new AWS.S3();
+
+module.exports.handle = async ({ Records: records }, context) => {
+  try {
+    await Promise.all(records.map(async record => {
+      console.log(record);
+      const { key } = record.s3.object;
+
+      const dataImage = await S3.getObject({
+        Bucket: process.env.bucket,
+        Key: key,
+      }).promise();
+
+      const optimized = await sharp(dataImage.Body)
+        .resize(1280, 720, { fit: 'inside', withoutEnlargement: true})
+        .toFormat('jpeg', { progressive: true, quality: 50 })
+        .toBuffer()
+
+      await S3.putObject({
+        Body: optimized,
+        Bucket: process.env.bucket,
+        ContentType: 'image/jpeg',
+        Key: `compressed/${basename(key, extname(key))}.jpg`
+      }).promise()
+    }));
+
+    return {
+      statusCode: 301,
+      body: { ok: true }
+    }
+  } catch(err) {
+    return err;
+  }
+};
